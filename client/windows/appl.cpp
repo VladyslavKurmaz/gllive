0b2888e45931dbfc726ac7165c40ca12ad7b3105
vladyslav.kurmaz <vladyslav.kurmaz@globallogic.com>
2013-12-08 22:13:08 +0200

    main window centering

--- client/windows/appl.cpp
+++ client/windows/appl.cpp
@@ -15,7 +15,7 @@ appl::appl( std::ostream& l ) :
 		add_option( po_alignment,				"aligmnent,a",
 			boost::program_options::value<int>()->default_value(1),								"Side aligment", desc ).
 		add_option( po_transparency,			"transparency,t",
-			boost::program_options::value<unsigned char>()->default_value(256-32),					"Window trancparency", desc );
+			boost::program_options::value<unsigned char>()->default_value(255-64),					"Window trancparency", desc );
 }
 
 appl::~appl()
@@ -46,19 +46,19 @@ bool appl::init( int argc, char const * const argv[] )
 void appl::run() {
 	//
 	RECT rect;
-	DWORD const style = WS_OVERLAPPED;
+	DWORD const style = WS_OVERLAPPED | /*WS_CLIPCHILDREN | */WS_CLIPSIBLINGS;
 	DWORD const ex_style = WS_EX_TOPMOST | WS_EX_TOOLWINDOW | WS_EX_LAYERED;
 	struct _{
 		static bool __( WNDCLASSEX& wcex, CREATESTRUCT& cs, RECT const& rect, DWORD const style, DWORD const ex_style ) {
 			wcex.cbSize;
-			wcex.style;
+			wcex.style			=	0/*CS_HREDRAW | CS_VREDRAW*/;
 			wcex.lpfnWndProc;
 			wcex.cbClsExtra;
 			wcex.cbWndExtra;
 			wcex.hInstance;
 			wcex.hIcon;
 			wcex.hCursor;
-			wcex.hbrBackground	=	(HBRUSH)GetStockObject( GRAY_BRUSH );
+			wcex.hbrBackground	=	(HBRUSH)GetStockObject( BLACK_BRUSH );
 			wcex.lpszMenuName;
 			wcex.lpszClassName;
 			wcex.hIconSm;
@@ -78,11 +78,12 @@ void appl::run() {
 			return true;
 		}
 	};
-	SetRect( &rect, 0, 0, 640, 480 );
-	window::calc_rect( rect, style, ex_style, false, true );
+	SetRect( &rect, 0, 0, 64, 256 );
+	window::calc_rect( rect, style, ex_style, false, true, false );
 	if ( this->w.init( boost::bind( _::__, _1, _2, boost::ref( rect ), style, ex_style ), true ) )
 	{
 		this->w.set_styles( style, ex_style ).set_alpha( po.as< unsigned char >( po_transparency ) );
-		this->w.show( true ).run();
+		//CreateWindow( "BUTTON", "Push", WS_VISIBLE | WS_CHILD, 10, 10, 100, 20, this->w.get_hwnd(), NULL, NULL, 0 );
+		this->w.show( true ).invalidate().run();
 	}
 }
